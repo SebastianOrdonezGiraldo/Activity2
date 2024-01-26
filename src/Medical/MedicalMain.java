5c739559d182f40eb2ab2da04c69d26f13f8447b
Sebastian Ordoñez Giraldo <sebastian789go@gmail.com>
2024-01-26 18:45:49 -0500

    Crear citas okey

--- src/Medical/MedicalMain.java
+++ src/Medical/MedicalMain.java
@@ -11,15 +11,17 @@ public class MedicalMain {
     public static void main(String[] args) {
         Medical schedule = new Medical();
         Scanner lector = new Scanner(System.in);
-        System.out.println("Ingrese nombre del paciente:");
-        schedule.userName = lector.next();
-        System.out.println("Ingrese hora deseada:");
+        System.out.println("Bienvenido al sistema de citas medicas");
+        System.out.print("Nombre del paciente:");
+        schedule.userName = lector.nextLine();
+        System.out.print("Ingrese hora deseada:");
         schedule.hour = lector.nextFloat();
-        System.out.println("Ingrese fecha deseada formato 00/00/00:");
+        System.out.print("Ingrese fecha deseada formato 00/00/00:");
         schedule.date = lector.next();
         System.out.println("Cita agendada correctamente");
-        System.out.println("A nombre de:" + schedule.userName);
-        System.out.println("hora:" + schedule.hour);
-        System.out.println("fecha:" + schedule.date);
+        System.out.println("A nombre de: " + schedule.userName);
+        System.out.println("hora: " + schedule.hour);
+        System.out.println("fecha: " + schedule.date);
     }
+
 }
