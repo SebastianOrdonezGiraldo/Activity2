c1379f78cf8f28c5f7df3d1a42c606b4fbbd1eb2
Sebastian Ordoñez Giraldo <sebastian789go@gmail.com>
2024-02-05 22:21:30 -0500

    Subiendo ejercicio 6

--- src/PagoEmpleado/Pago.java
+++ src/PagoEmpleado/Pago.java
@@ -1,9 +1,74 @@
 package PagoEmpleado;
 
 public class Pago {
-    public int codigoEmpleado;
-    private double salarioBase;
-    public double horasExtras;
 
+    private int codEmpleado;
+    private double salarioBasico;
+    private double horasExtras;
+    private int numeroHoras;
+    private double descuento;
+    private double descuentoPrestamo;
+    private double bonificacion;
+    private double neto;
+    public Pago(){}
+    public Pago(int codEmpleado, double salarioBasico, double horasExtras, int numeroHoras, double descuento, double descuentoPrestamo, double bonificacion) {
+        this.codEmpleado = codEmpleado;
+        this.salarioBasico = salarioBasico;
+        this.horasExtras = horasExtras;
+        this.numeroHoras = numeroHoras;
+        this.descuento = descuento;
+        this.descuentoPrestamo = descuentoPrestamo;
+        this.bonificacion = bonificacion;
+        calcularNeto();
+    }
 
+    public void calcularValorHE() {
+        double valorHora = salarioBasico / (30 * 8);
+        horasExtras = numeroHoras * valorHora * 1.5;
+    }
+
+    public double calcularDevengado() {
+        return salarioBasico + horasExtras + bonificacion;
+    }
+
+    public double calcularDeducido() {
+        return descuento + descuentoPrestamo;
+    }
+
+    public void calcularNeto() {
+        neto = calcularDevengado() - calcularDeducido();
+    }
+
+    // Métodos getter y setter para los atributos privados
+    public double getHorasExtras() {
+        return horasExtras;
+    }
+
+    public void setHorasExtras(double horasExtras) {
+        this.horasExtras = horasExtras;
+    }
+
+    public double getSalarioBasico() {
+        return salarioBasico;
+    }
+
+    public void setSalarioBasico(double salarioBasico) {
+        this.salarioBasico = salarioBasico;
+    }
+
+    public double getDescuento() {
+        return descuento;
+    }
+
+    public void setDescuento(double descuento) {
+        this.descuento = descuento;
+    }
+
+    public double getNeto() {
+        return neto;
+    }
+
+    public void setNeto(double neto) {
+        this.neto = neto;
+    }
 }
--- src/PagoEmpleado/PagoMain.java
+++ src/PagoEmpleado/PagoMain.java
@@ -1,4 +1,25 @@
 package PagoEmpleado;
 
+import javax.swing.*;
+
 public class PagoMain {
+    public static void main(String[] args) {
+        Pago pago = new Pago();
+        String valorHE = JOptionPane.showInputDialog("Ingrese el valor de las horas extras:");
+        pago.setHorasExtras(Double.parseDouble(valorHE));
+        pago.calcularValorHE();
+
+        String devengado = JOptionPane.showInputDialog("Ingrese el valor devengado:");
+        pago.setSalarioBasico(Double.parseDouble(devengado));
+
+        String deducido = JOptionPane.showInputDialog("Ingrese el valor deducido:");
+        pago.setDescuento(Double.parseDouble(deducido));
+
+        String neto = JOptionPane.showInputDialog("Ingrese el valor neto:");
+        pago.setNeto(Double.parseDouble(neto));
+        pago.calcularNeto();
+
+        JOptionPane.showMessageDialog(null, "Datos ingresados:\nValor HE: " + pago.getHorasExtras() +
+                "\nDevengado: " + pago.getSalarioBasico() + "\nDeducido: " + pago.getDescuento() + "\nNeto: " + pago.getNeto());
+    }
 }
